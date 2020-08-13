bd694cff0f2acad16f9f22bd120b13ba69f01ed9
Mh69610 <69611458+Mh69610@users.noreply.github.com>
2020-08-12 18:37:38 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("One of my favorite games is ROBLOX");
+		System.out.println("One of my hobbies is playing the piano");
+		System.out.println("I feel like I should major in music theory");
+		System.out.println("One of my favorite foods is pizza");
+		System.out.println("I have an interest in cooking");
+	}
+}
